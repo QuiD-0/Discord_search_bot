cd8247c8e30ab641f403a2f9c7dcecd239b364e3
QuiD <wodnd101@naver.com>
2021-03-01 17:21:44 +0900

    각인 추가
    캐릭터 검색시 각인 정보 추가

--- loa.py
+++ loa.py
@@ -20,18 +20,29 @@ def search(message):
             lv_nick=soup.select_one('div.pl-2.pr-2.pt-0.pb-0.m-0 > p > span.bg-theme-8.rounded.shadow-sm.text-theme-1.tfs18 > strong').text
             name=soup.find_all('span',{'class':'badge badge-pill bg-theme-8 text-theme-1 tfs14 w500'})
             info=soup.find_all('span',{'class':'text-theme-0 tfs14'})
-            embed = discord.Embed(title=lv_nick,url=url)
+            embed = discord.Embed(title=lv_nick,url=url, color=0x000000)
             embed.set_thumbnail(url="attachment://image.png")
             for i in range(0,6):
                 embed.add_field(name=name[i].get_text(), value=info[i].get_text(), inline=True)
+            
+            gackin=[]
+            gack_active = soup.find('a',{'class':'btn-theme-4 text-right rounded link-theme-4 p-1 active'})
+            gackin_tag = soup.find_all('a',{'class':'btn-theme-4 text-right rounded link-theme-4 p-1'})
+            gackin.append(gack_active.get_text())
+            if gackin_tag:
+                for tag in gackin_tag:
+                    gackin.append(tag.get_text())
+                embed.add_field(name="각인 정보", value='\n'.join(gackin), inline=False)
+            else:
+                embed.add_field(name="각인 정보", value='각인 없음', inline=False)
 
-            return embed,image
+
         except:
             embed = discord.Embed(title='검색 결과 없음', description=nickname)
             return embed,None
 
     else : 
         embed = discord.Embed(title='404 NOT FOUND', description='잠시후 다시 시도해주세요.')
-        return embed,image
+        return embed,None
     
     return embed,image
